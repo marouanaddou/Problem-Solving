a3d6d192bbde60fbbfa967aff131cfec3c84dff9
marouanaddou <marwanaddou123@gmail.com>
2024-03-09 16:44:30 +0100

    minimum Common Value

---
+++ minimumCommonValue/main.cpp
@@ -0,0 +1,10 @@
+
+
+# include "minimumCommonValue.hpp"
+
+int main()
+{
+    vector<int> nums1{1,1,2,3};
+    vector<int> nums2{2,3};
+    cout << Solution::getCommon(nums1, nums2); // 2
+}
---
+++ minimumCommonValue/minimumCommonValue.hpp
@@ -0,0 +1,13 @@
+
+// 2540
+
+# pragma one 
+# include <iostream>
+# include <vector>
+using namespace std;
+
+class Solution 
+{
+    public:
+        static int getCommon(vector<int>& nums1, vector<int>& nums2);
+};
