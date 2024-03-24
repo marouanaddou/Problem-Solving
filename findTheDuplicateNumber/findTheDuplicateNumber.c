ffcb916cdd461923e3a6d168cec0ff076e3bb933
marouanaddou <marwanaddou123@gmail.com>
2024-03-24 00:53:58 +0000

    Find The Duplicate Number

---
+++ findTheDuplicateNumber/findTheDuplicateNumber.c
@@ -0,0 +1,23 @@
+
+# include <stdio.h>
+
+int findDuplicate(int* nums, int numsSize) 
+{
+    int *arr = (int *)calloc(numsSize , sizeof(int));  // Allocate memory for a new array and initialize it with values set to 0.
+    for ( int i = 0; i < numsSize; i++ )
+    {
+        arr[nums[i]]++;
+        if ( arr[nums[i]] >= 2 ) return nums[i];
+    }
+    return 0;
+}
+
+/********************** TEST ***********************/
+
+// int main()
+// {
+//     int arr[] = {1,2,3,4,2};
+//     printf ("%d\n", findDuplicate(arr, 5));
+// }
+
+/********************** TEST ***********************/
