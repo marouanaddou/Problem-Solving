83968a8cfe71b93cb0fa5204a31a026bb094aeb3
Marouane Addou <maddou@c1r8p2.1337.ma>
2024-06-01 01:44:13 +0100

    Calculate the sum of the ASCII differences between characters in a string

---
+++ ScoreOfAString/ScoreOfAString.cpp
@@ -0,0 +1,17 @@
+
+# include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int scoreOfString(string s);
+};
+
+int Solution::scoreOfString(string s)
+{
+    int sum = 0;
+    for (int i = 1; i < s.size(); i++)
+        sum += abs(s[i - 1] - s[i]);
+    return sum;
+}
